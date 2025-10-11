4dfc1f75e5f45fb0fa1badd0ebf7d886cc69c611
Li Kang <li.kang@teletracnavman.com>
2025-10-12 10:35:58 +1300

    localization using static locale

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,14 +1,27 @@
 //server side JS will run on Build Time
 const axios = require('axios');
+const countries = require('./countries.json');
+
 require('dotenv').config();
 
-module.exports = async function () {
+async function getNews(country) {
   try {
     const response = await axios.get(
-      `https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`
+      `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`
     );
-    return response.data;
+    return {
+      country,
+      articles: response.data.articles,
+    };
   } catch (error) {
     console.error(error);
   }
+}
+
+module.exports = async function () {
+  const newPromises = countries.map(getNews);
+  return Promise.all(newPromises).then((newsByCountries) => {
+    console.log('newsByCountries:', newsByCountries);
+    return Array.prototype.concat.apply([], newsByCountries);
+  });
 };
