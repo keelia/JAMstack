ee98e45de3951c20aa8bbe38516924de06481ad7
Li Kang <li.kang@teletracnavman.com>
2025-10-15 13:27:03 +1300

    Update weather_api.js

--- src/_functions/weather_api.js
+++ src/_functions/weather_api.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+// require('dotenv').config();
 
 exports.handler = async function (event, context) {
   const data = getWeather(event.queryStringParameters);
@@ -12,9 +13,11 @@ exports.handler = async function (event, context) {
 async function getWeather({ lat, lon }) {
   try {
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}&units=metric`;
+    console.log('get weather', process.env.WEATHER_API_KEY);
     const response = await axios.get(url);
     return response.data;
   } catch (error) {
+    console.error(error);
     return {};
   }
 }
