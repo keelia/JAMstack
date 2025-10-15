00287fcb006a5ebc30dbf315b9e2e179bcb22755
Li Kang <li.kang@teletracnavman.com>
2025-10-15 13:08:55 +1300

    client side fetch weather data from serverless function runing on netlify

---
+++ src/_functions/weather_api.js
@@ -0,0 +1,20 @@
+const axios = require('axios');
+
+exports.handler = async function (event, context) {
+  const data = getWeather(event.queryStringParameters);
+  // your server-side functionality
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data),
+  };
+};
+
+async function getWeather({ lat, lon }) {
+  try {
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}&units=metric`;
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    return {};
+  }
+}
